7f0c8e7d9a05910eb2e7e26afe4a66b5b74dd5c6
Aidan-bn <bantezea@gmail.com>
2024-02-12 12:30:59 +0300

    Start working with header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Nav from './components/Header'
 
 function App() {
 
   return (
     <>
+    <Nav />
       <div>
         Classroom Web
       </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+const Header = () => {
+    return(
+        <>
+            <header>
+                <nav>
+                    <h1>
+                        Logo
+                    </h1>
+                    <menu>
+                        <li>Home</li>
+                        <li>Primary</li>
+                        <li>O-Level</li>
+                        <li>A-Level</li>
+                        <li>Remote Teacher</li>
+                        <li>Others</li>
+                    </menu>
+                    <h2>
+                        menu
+                    </h2>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header;
