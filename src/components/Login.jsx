f7d383b20ad8200e0d0ed05eb50c3eb05ff7dc5e
Aidan-bn <bantezea@gmail.com>
2024-03-05 00:08:48 +0300

    Create a login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,34 @@ const Login = () => {
     <>
        <div className="primary">
             <div className="div-primary">
-            <h1>Login for better experience</h1>
+              <h3 className="div-title">Login for better experience</h3>
+              <div className="login-div">
+                <form>
+                    <label>
+                        User name:
+                        <input 
+                            type="text" 
+                            placeholder="Admin"
+                            name="user-name"
+                        />
+                    </label>
+                    <label>
+                        Password:
+                        <input 
+                            type="password" 
+                            placeholder="******"
+                            name="user-name"
+                        />
+                    </label>
+                    <button>
+                        Log In
+                    </button>
+                </form>
+                <p>
+                    By signing In, you will be able brouwse all
+                    our reading materials
+                </p>
+              </div>
             </div>
         </div>
     </>
