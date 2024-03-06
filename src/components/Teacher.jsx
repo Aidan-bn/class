a35403325a04102dbd290ac79fd513f3a3afaae6
Aidan-bn <bantezea@gmail.com>
2024-03-06 23:55:18 +0300

    Update teacher booking page

--- src/components/Teacher.jsx
+++ src/components/Teacher.jsx
@@ -1,12 +1,71 @@
+import { useState } from "react";
+import { classLists } from "../menu/classList";
+import pic  from "../assets/student.jpg";
+
 const Teacher = () => {
+  const [ show, setShow ] = useState(true);
+
+  const switchContent = () => {
+    setShow((prev) => !prev)
+  }
+  
     return (
-      <div className="primary">
+      <div className="primary ">
+        {/* center */}
           <div className="div-primary">
-           <h1>Hello Teacher</h1>
+           <h1>Book a teacher</h1>
+           <div className="main-primary">
+              <aside>
+                <menu
+                  className="teacher-list">
+                  {classLists.map((list) => 
+                    <li 
+                      key='id'
+                      onClick={switchContent}
+                      >
+                        {list.name}
+                    </li>
+                  )}
+                </menu>
+              </aside>
+              <aside>
+                {
+                  show &&
+                  <div
+                    className="teacher-box"
+                    >
+                    {
+                      classLists.map((subject) =>
+                        <div 
+                          key='id' 
+                          className="teacher-details"
+                        >
+                          {/* <img src={subject.available.pic} alt="teacher-image" className="avatar" /> */}
+                          <img src={pic} alt="teacher-image" className="avatar" />
+                          <div 
+                            className="teacher-info"
+                          >
+                            <h5>{subject.available.name}</h5>
+                            <h5>{subject.available.subjects}</h5> 
+                            <h5>{subject.available.contact}</h5>
+                            <p>{subject.available.bio}</p>
+                            <button
+                              className="btn-booking"
+                              >
+                              Booking
+                            </button>
+                          </div>
+                        </div>
+                      )
+                    }
+                  </div>
+                }
+              </aside>
+           </div>
           </div>
       </div>
     );
   }
   
-  export default Teacher
+  export default Teacher;
   
---
+++ src/menu/classList.js
@@ -0,0 +1,77 @@
+export const classLists = [
+    {
+        id: 1,
+        name: "Primary",
+        url: '/primary-teachers',
+        available: {
+            name: 'Aidan Banteze',
+            subjects: 'Maths, Physics',
+            pic: '../assets/student.jpg',
+            contact: '075312220',
+            bio: 'University Graduate '
+        }
+    },
+    {
+        id: 2,
+        name: "Olevel",
+        url: '/olevel-teachers',
+        available: {
+            name: 'Aidan Banteze',
+            subjects: 'Geography, Physics',
+            pic: './asset/img.jpg',
+            contact: '075312220',
+            bio: 'A University Graduate '
+        }
+    },
+    {
+        id: 3,
+        name: "Alevel",
+        url: '/alevel-teachers',
+        available: [
+            {
+            name: 'Flavian Banteze',
+            subjects: 'Maths, Chemistry',
+            pic: './asset/img1.jpg',
+            contact: '075312220',
+            bio: 'A University Graduate '
+            },
+            {
+            name: 'Aidan Banteze',
+            subjects: 'Maths, Physics',
+            pic: './asset/img.jpg',
+            contact: '075312220'
+            },
+            {
+            name: 'Aidan Habayo',
+            subjects: 'ICT, Physics',
+            pic: './asset/img.jpg',
+            contact: '075312220'
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: "Sports",
+        url: '/olevel-teachers',
+        available: {
+            name: 'Aidan Banteze',
+            subjects: 'Geography, Physics',
+            pic: './asset/img.jpg',
+            contact: '075312220',
+            bio: 'A University Graduate '
+        }
+    },
+    {
+        id: 5,
+        name: "Creativity",
+        url: '/olevel-teachers',
+        available: {
+            name: 'Aidan Banteze',
+            subjects: 'Geography, Physics',
+            pic: './asset/img.jpg',
+            contact: '075312220',
+            bio: 'A University Graduate '
+        }
+    }
+ 
+]
